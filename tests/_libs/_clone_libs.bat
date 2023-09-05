339d3435fab2a03e1fa6c66fa41592b99914096a
al-martyn1 <amart@mail.ru>
2023-09-06 02:49:30 +0300

    Убрал излишки от большого проекта

--- tests/_libs/_clone_libs.bat
+++ tests/_libs/_clone_libs.bat
@@ -26,19 +26,9 @@ set PREFIX=git@%BASE%:%ACC%
 :DO_CLONE
 
 git clone %PREFIX%/umba_mm_mod_encodings.git   %~dp0\encoding
-rem git clone %PREFIX%/marty_adt.git               %~dp0\marty_adt
 git clone %PREFIX%/marty_cpp.git               %~dp0\marty_cpp
-rem git clone %PREFIX%/marty_csv.git               %~dp0\marty_csv
-rem git clone %PREFIX%/marty_dc_impl_win32.git     %~dp0\marty_dc_impl_win32
 git clone %PREFIX%/marty_decimal.git           %~dp0\marty_decimal
 git clone %PREFIX%/marty_draw_context.git      %~dp0\marty_draw_context
-rem git clone %PREFIX%/marty_fs_adapters.git       %~dp0\marty_fs_adapters
-rem git clone %PREFIX%/marty_pugixml.git           %~dp0\marty_pugixml
-rem git clone %PREFIX%/marty_rcfs.git              %~dp0\marty_rcfs
-rem git clone %PREFIX%/marty_rich_text.git         %~dp0\marty_rich_text
-rem git clone %PREFIX%/marty_tr.git                %~dp0\marty_tr
-rem git clone %PREFIX%/marty_utf.git               %~dp0\marty_utf
-rem git clone %PREFIX%/marty_yaml_toml_json.git    %~dp0\marty_yaml_toml_json
 git clone %PREFIX%/umba_mm_mod_sfmt.git        %~dp0\sfmt
 git clone %PREFIX%/umba_mm_mod_umba.git        %~dp0\umba
 
--- tests/_libs/_git-job-impl.bat
+++ tests/_libs/_git-job-impl.bat
@@ -1,18 +1,7 @@
-rem @call :DO_GIT_JOB %1 %~dp0\2c_common
 @call :DO_GIT_JOB %1 %~dp0\encoding
-rem @call :DO_GIT_JOB %1 %~dp0\marty_adt
 @call :DO_GIT_JOB %1 %~dp0\marty_cpp
-rem @call :DO_GIT_JOB %1 %~dp0\marty_csv
-rem @call :DO_GIT_JOB %1 %~dp0\marty_dc_impl_win32
 @call :DO_GIT_JOB %1 %~dp0\marty_decimal
 @call :DO_GIT_JOB %1 %~dp0\marty_draw_context
-rem @call :DO_GIT_JOB %1 %~dp0\marty_fs_adapters
-rem @call :DO_GIT_JOB %1 %~dp0\marty_pugixml
-rem @call :DO_GIT_JOB %1 %~dp0\marty_rcfs
-rem @call :DO_GIT_JOB %1 %~dp0\marty_rich_text
-rem @call :DO_GIT_JOB %1 %~dp0\marty_tr
-rem @call :DO_GIT_JOB %1 %~dp0\marty_utf
-rem @call :DO_GIT_JOB %1 %~dp0\marty_yaml_toml_json
 @call :DO_GIT_JOB %1 %~dp0\sfmt
 @call :DO_GIT_JOB %1 %~dp0\umba
 
