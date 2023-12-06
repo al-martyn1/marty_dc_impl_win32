9895055c16f0cc314ed9d86f7bac8925700b8d31
al-martyn1 <amart@mail.ru>
2023-12-06 21:19:56 +0300

    Добавил тестов

---
+++ tests/src/test_simplesquirrel03.cpp
@@ -0,0 +1,42 @@
+/*! \file
+    \brief Test for aset manager
+*/
+
+#include "umba/umba.h"
+//
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <list>
+#include <queue>
+#include <deque>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <iterator>
+#include <random>
+#include <cstdlib>
+#include <exception>
+#include <stdexcept>
+#include <functional>
+#include <utility>
+
+
+// #pragma warning(disable:4717)
+
+
+
+
+int main( int argc, char* argv[] )
+{
+    UMBA_USED(argc);
+    UMBA_USED(argv);
+
+
+}
+
---
+++ tests/src/test_simplesquirrel04.cpp
@@ -0,0 +1,51 @@
+/*! \file
+    \brief Test for squirrel-lang "import" extention
+*/
+
+#include "umba/umba.h"
+//
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <list>
+#include <queue>
+#include <deque>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <iterator>
+#include <random>
+#include <cstdlib>
+#include <exception>
+#include <stdexcept>
+#include <functional>
+#include <utility>
+
+
+// #pragma warning(disable:4717)
+
+#include "simplesquirrel/simplesquirrel.hpp"
+#include "marty_simplesquirrel/simplesquirrel.h"
+
+
+#ifdef _MSC_VER
+#pragma comment(lib, "_squirrel.lib")
+#pragma comment(lib, "_simplesquirrel.lib")
+#endif
+
+
+
+
+int main( int argc, char* argv[] )
+{
+    UMBA_USED(argc);
+    UMBA_USED(argv);
+
+
+}
+
